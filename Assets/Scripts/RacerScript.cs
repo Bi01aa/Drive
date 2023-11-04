ea74270d3f7a545be33e09e81aac72070c57bd6c
bi28xo <bi28xo@student.sunderland.ac.uk>
2023-11-04 12:53:27 +0000

    Added Speedometer (needs bugfixing)
    I cant do that bugfixing myself due to reasons mentioned on Teams chat

--- Assets/Scripts/RacerScript.cs
+++ Assets/Scripts/RacerScript.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RacerScript : MonoBehaviour
 {
+    [SerializeField] private Text SpeedText;
 
     public float laptime = 0;
     public float besttime = 0;
@@ -24,7 +26,7 @@ public class RacerScript : MonoBehaviour
 
             Ltime.text = "Time: " + laptime.ToString("F2");
         }
-        
+        SpeedText.text = (m_Rigidbody.Velocity.Magnitude * 2.23693629f).ToString("0")+ (" MPH");
     }
 
     private void OnTriggerEnter(Collider other)
